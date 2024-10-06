dc1383e6835c2a489d834f056b6d2cdcb8c64637
gd man <real.gd.man@gmail.com>
2024-10-06 18:58:26 +0300

    add bgr shader

--- scenes/viewport_scene.gd
+++ scenes/viewport_scene.gd
@@ -1,4 +1,25 @@
 extends Node2D
 
 func _ready():
-	$"%VJoystick".connect("vjoy_move", $Viewport/game_scene/test_player, "on_vjoy_move")
+	var _e = $"%VJoystick".connect("vjoy_move", $Viewport/game_scene/test_player, "on_vjoy_move")
+
+	_e = $"%BtnRbgMode".connect("pressed", self, "on_rgb_mode_pressed")
+	
+	set_rgb_mode(0)
+
+var current_rgb_mode = 0
+
+var rgb_modes = [
+	{ "shader": "res://util/subpixel/subpixel_canvas_rgb.gdshader",
+	"label": "RGB\n[color=red]I[/color][color=green]I[/color][color=blue]I[/color]"},
+	{ "shader": "res://util/subpixel/subpixel_canvas_bgr.gdshader",
+	"label": "BGR\n[color=blue]I[/color][color=green]I[/color][color=red]I[/color]"},
+]
+
+func on_rgb_mode_pressed():
+	current_rgb_mode = wrapi(current_rgb_mode + 1, 0, rgb_modes.size())
+	set_rgb_mode(current_rgb_mode)
+	
+func set_rgb_mode(index):
+	($SubpixelCanvas.material as ShaderMaterial).shader = load(rgb_modes[index].shader)
+	$GUI/LblRbgMode.bbcode_text = rgb_modes[index].label
