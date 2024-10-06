396cdefad1410de7f38c1442ffc7676d8da03c6d
gd man <real.gd.man@gmail.com>
2024-10-06 05:05:10 +0300

    refactor scenes

--- scenes/menu_scene.gd
+++ scenes/menu_scene.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
 func _ready():
-	var _e = $"%BtnStart".connect("pressed", get_tree(), "change_scene", ["res://scenes/game_scene.tscn"])
+	var _e = $"%BtnStart".connect("pressed", get_tree(), "change_scene", ["res://scenes/viewport_scene.tscn"])
